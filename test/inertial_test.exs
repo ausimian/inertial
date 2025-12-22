c4d3f9a1466cdc0502457fe1eb4044919fe8deee
ausimian <nick@ausimian.net>
2025-12-23 10:51:11 +1100

    Add workflow

--- test/inertial_test.exs
+++ test/inertial_test.exs
@@ -1,3 +1,12 @@
 defmodule InertialTest do
   use ExUnit.Case
+
+  test "can subscribe and receive events" do
+    ref = Inertial.subscribe()
+    assert is_reference(ref)
+    # Since we can't easily trigger real network events in a test,
+    # we will just check that the subscription works and that no
+    # messages are received immediately.
+    refute_receive {^ref, _event}, 100
+  end
 end
